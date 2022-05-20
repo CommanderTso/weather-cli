da2d2040c784332ae523d33e9a92163b821eec01
Scott Macmillan <CommanderTso@users.noreply.github.com>
2022-05-20 16:02:23 -0400

    Fully working MVP of weather command line app

--- bin/index.js
+++ bin/index.js
@@ -1,37 +1,31 @@
 #! /usr/bin/env node
 
-const yargs = require("yargs")
 const https = require("https")
 const dotenv = require("dotenv")
-
 dotenv.config()
 
-const usage = "\nUsage: weather <zipcode>"
-const options = yargs
-  .usage(usage)
-  // .option("--set-default", {describe: "Set default zipcode", type: ""})
+const yargs = require("yargs")
+  .usage("\nUsage: weather <zipcode>")
   .help(true)
-  .argv
+  .check((argv) => {
+    if (isNaN(argv._))
+      throw new Error("Weather must be called with one valid US zipcode as its argument.")
+    else
+      return true
+  })
 
-yargs.check((argv) => {
-  if (isNaN(argv._))
-    throw new Error("Weather must be called with one valid US zipcode as its argument.")
-  else
-    return true
-})
-
-function makeAPICall() {
+function makeAPICall(zipcode, apiKey) {
   const httpOptions = {
     hostname: "api.openweathermap.org",
-    path: `/data/2.5/weather?zip=01338,us&appid=${process.env.WEATHER_API_KEY}`,
+    path: `/data/2.5/weather?zip=${zipcode},us&units=imperial&appid=${apiKey}`,
     method: "POST"
   }
 
   const req = https.request(httpOptions, res => {
-    console.log(`statusCode: ${res.statusCode}`);
-
-    res.on('data', d => {
-      process.stdout.write(d);
+    res.setEncoding('utf8')
+    res.on('data', data => {
+      console.log(data)
+      outputResults(JSON.parse(data), zipcode)
     });
   })
 
@@ -42,4 +36,17 @@ function makeAPICall() {
   req.end();
 }
 
-makeAPICall()
+function outputResults(data, zipcode) {
+  const visibility = data.visibility = 10000 ? "Effectively Unlimited" : `${data.visibility} ft.`
+
+  const output = `Here's the weather for ${data.name} (${zipcode}):
+
+  Overall: ${data.weather[0].main} (${data.weather[0].description})  
+  Temperature: ${data.main.temp}°F
+  Visiblity: ${visibility}
+  `
+
+  console.log(output)
+}
+
+makeAPICall(yargs.argv._[0], process.env.WEATHER_API_KEY)
